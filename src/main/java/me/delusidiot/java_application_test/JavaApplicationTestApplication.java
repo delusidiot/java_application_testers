f9b8211450ac00511af27ea42ba6da67a650db47
delusidiot <delusidiot@gmail.com>
2021-12-03 11:29:13 +0900

    Initial commit

---
+++ src/main/java/me/delusidiot/java_application_test/JavaApplicationTestApplication.java
@@ -0,0 +1,13 @@
+package me.delusidiot.java_application_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaApplicationTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaApplicationTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/delusidiot/java_application_test/JavaApplicationTestApplicationTests.java
@@ -0,0 +1,13 @@
+package me.delusidiot.java_application_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaApplicationTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
